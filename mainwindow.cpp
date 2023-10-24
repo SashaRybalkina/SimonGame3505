e51f81149ebd0b655265f8796787ab5886d13e73
sasharybalkina <sashaawesomeness@gmail.com>
2023-10-24 13:08:41 -0600

    Added comments to the code to make it easier to understand

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,8 +21,15 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
                                           "QPushButton:pressed {background-color: rgb(250,215,150);}"));
     connect(ui->startButton, &QPushButton::clicked, this, &model::startButtonClickedSlot);
 
-    connect(&model, &model::flashColor, this);
+    connect(&model, &model::flashColor, this, &MainWindow::flash(flashColor));
+    //              signal recieved     affects this class      uses the flash slot.
+
     connect(&model, &model::disableStart, ui->startButton, &QPushButton::setDisabled);
+    //              signal recieved       button affected                action, in this case disabling the start button
+
+    connect(&model, &model::disableStart, ui->blueButton, &QPushButton::setDisabled);
+
+    connect(&model, &model::disableStart, ui->redButton, &QPushButton::setDisabled);
 
     gameOver = false;
 
@@ -33,8 +40,24 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//This is the slot that is supposed to get called when a signal to flash colors is sent.
 void MainWindow::flash(int colorCode)
 {
-
+    if (colorCode == 0)
+    {
+        //flash blue
+        ui->blueButton->setStyleSheet( QString("QPushButton {background-color: rgb(200,200,250);}"
+                                    "QPushButton:pressed {background-color: rgb(150,150,255);}"));
+        ui->blueButton->setStyleSheet( QString("QPushButton {background-color: rgb(100,100,250);}"
+                                    "QPushButton:pressed {background-color: rgb(150,150,255);}"));
+    }
+    else
+    {
+        //flash red
+        ui->redButton->setStyleSheet( QString("QPushButton {background-color: rgb(250,200,200);}"
+                                    "QPushButton:pressed {background-color: rgb(255,150,150);}"));
+        ui->redButton->setStyleSheet( QString("QPushButton {background-color: rgb(250,100,100);}"
+                                    "QPushButton:pressed {background-color: rgb(255,150,150);}"));
+    }
 }
 
--- model.cpp
+++ model.cpp
@@ -14,10 +14,18 @@ model::model(QObject *parent)
 }
 void model::startButtonClickedSlot()
 {
+    //This sends the signal to MainWindow to make all buttons disabled
     emit disableStart(true);
+
+    //This will be the random pattern of button flashes
     vector<int> pattern;
+
+    //The first round will have three button flashes
     int patternLength = 3;
+
     QTimer timer(this);
+
+    //This is where I generate the pattern for the buttons to flash. 0 = blue and 1 = red.
     while (patternLength > 0)
     {
         pattern.push_back(arc4random() % 2);
@@ -28,23 +36,21 @@ void model::startButtonClickedSlot()
     {
         for (int i = 0; i < pattern.size(); i++)
         {
+            //for every color code in the pattern, we will delay each color flash by a second
+            //and emit a flashColor signal with the current color code passed in as a parameter
+            //to let MainWindow know that the flash slot needs to be called.
             QTimer::singleShot(i * 1000, this, [=] () {
                 emit flashColor(pattern[i]);
             });
-//            if (pattern[i] == 0)
-//            {
-//                //light up blue
-//                ui->blueButton->animateClick();
-//            }
-//            else
-//            {
-//                //light up red
-//                ui->redButton->animateClick();
-//            }
         }
 
+        //increases the pattern by one every round
         pattern.push_back(arc4random() % 2);
 
+        //This is just the logic for invoking a game over. For now it will just break out of the
+        //loop after the first iteration. In the future, each iteration of the loop will be a new
+        //round, and once the player makes a mistake, this if statement will be invoked to send a
+        //game over signal and end the game.
         if (true)
         {
             break;
