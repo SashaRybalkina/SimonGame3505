88cde235df3743d257b4719ce16cecbfd9efd38a
Yeon <76601570+yeonkim1213@users.noreply.github.com>
2023-10-27 18:47:49 -0600

    Add short description of the creative element

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 /**
  * Main setup
- * @author Sasha Rybalkina
- * @author Yeon Kim
+ * @author Sasha Rybalkina - @crazyrussian123456
+ * @author Yeon Kim - @yeonkim1213
  * @date October 27, 2023
+ *
+ * [Short description of the creative element]
+ * Our Simon Game has a feature of "speed slider".
+ * Slider allows the user to adjust the speed of the game by sliding the button.
+ * The user can adjust the speed in 11 different modes.
+ * Both beginners and experts can enjoy our Simon Game!
  */
 
 #include "mainwindow.h"
